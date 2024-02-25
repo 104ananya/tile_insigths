98ff75def2ccbc15f61d17735f7079fea290b53d
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-25 13:59:34 +0530

    Structure and Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TileComponent } from './components/tile/tile.component';
+import { InsightsComponent } from './components/insights/insights.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent, TileComponent, InsightsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
