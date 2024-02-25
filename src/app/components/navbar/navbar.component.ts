63468b07824ced32343c60a716ed99d4fda32292
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2024-02-25 15:01:15 +0530

    Navbar & conditional rendering of Menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    // console.log('isMenuOpen:', this.isMenuOpen);
+  }
 }
